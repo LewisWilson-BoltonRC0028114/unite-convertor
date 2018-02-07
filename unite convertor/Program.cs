35c19dc447e64fec6f5fc070c33d83ebed52405e
rc0028114 <rc0028114@CRO-ICF07-20N>
2018-02-07 13:57:53 +0000

    hi

--- unite convertor/Program.cs
+++ unite convertor/Program.cs
@@ -16,7 +16,7 @@ namespace unite_convertor
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form1()); 
         }
     }
 }
